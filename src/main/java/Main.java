698413ee11080d5ff18c40af0008f75350df4755
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-03-02 14:34:06 -0500

    Finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,19 +2,31 @@
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer on Jan 2024.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
         // create a new object of class Main
+        Main newObject = new Main();
 
+        newObject.compute();
         // call the compute() method on that new object
     }
 
     public void compute() {
         // Get input string of name from user
-        
-        // Create conditional to check name against Alice and Bob
 
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("Enter your name here: ");
+         String input = scanner.nextLine().toLowerCase();
+        // Create conditional to check name against Alice and Bob
+         if(input.equals("alice") || input.equals("bob")){
+             System.out.println("Hey, "+input.substring(0,1).toUpperCase() + input.substring(1).toLowerCase()+" how are you doing today??");
+         }else{
+             System.out.println(input.substring(0,1).toUpperCase() + input.substring(1).toLowerCase() + " you are neither Alice nor Bob !");
+         }
         // Print greeting to screen if Alice or Bob are true
         
         // Print "You are neither Alice nor Bob." otherwise
